7ffc602229ab3ca6688ac5f0200e2d10a7a42467
Wei <weihengjiang255@gmail.com>
2023-10-04 12:35:13 -0400

    add server dump

---
+++ lab5/server_dump.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server Dump</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    <h1>3.2.2 Server Dump</h1>
+    <pre><?= var_dump($_SERVER) ?></pre>
+</body>
+</html>
